ee3a26e966080aee9ba79b00ab478a3644d62d0d
Faruk Hesenjan <fh522@vt.edu>
2021-08-04 15:01:37 -0400

    made few comments

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("work1 started");
         System.out.println("Created Git Repository");
+        System.out.println("Made a few comments");
     }
 }
